515fa956b1bbe91ab8f8defb0dacd6180282aaaa
calexil <calexil@yahoo.com>
2017-10-08 20:32:21 -0400

    more framework

--- assets/Tatakai.py
+++ assets/Tatakai.py
@@ -1,3 +1,7 @@
 import pyglet
 from pyglet.gl import glViewport, glMatrixMode, glOrtho, glLoadIdentity, glScalef
 from pyglet.gl import GL_PROJECTION, GL_MODELVIEW
+import pygame
+from pygame import scene, model, controller
+
+
